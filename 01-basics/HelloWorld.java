ac3d0085a66d16dbebddeb1042f7a89733ab43ce
Chiraggalav0024 <chirag.galav0024@gmail.com>
2026-08-25 16:28:09 +0530

    feat(basics): program for HelloWorld

---
+++ 01-basics/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello everybody.");
+        System.out.println("My name is Chirag.");
+        System.out.println("And, i am practicing java basics.");
+    }
+}
